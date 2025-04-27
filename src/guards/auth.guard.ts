992f16d7b29e77cbb4929944d19562291de68683
Efrat-Lavi <efratl3164@gmail.com>
2025-04-27 15:33:56 +0300

    auth

--- src/guards/auth.guard.ts
+++ src/guards/auth.guard.ts
@@ -1,15 +1,28 @@
+// import { inject } from '@angular/core';
+// import { CanActivateFn, Router } from '@angular/router';
+
+// export const authGuard: CanActivateFn = () => {
+//   console.log('🚨 Guard activated!'); // שורת Debug חשוב
+//   const router = inject(Router);
+//   const user = sessionStorage.getItem('user');
+//   console.log('👤 User from session:', user);
+
+//   if (!user) {
+//     router.navigate(['/login']);
+//     return false;
+//   }
+
+//   return true;
+// };
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 
 export const authGuard: CanActivateFn = () => {
-  console.log('🚨 Guard activated!'); // שורת Debug חשוב
   const router = inject(Router);
   const user = sessionStorage.getItem('user');
-  console.log('👤 User from session:', user);
 
   if (!user) {
-    router.navigate(['/login']);
-    return false;
+    return router.createUrlTree(['/login']);
   }
 
   return true;
